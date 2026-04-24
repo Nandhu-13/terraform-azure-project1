6f143f3afdd23a67e0d90f6dbc2f0faa187f1437
NandaKishore <parnatinanda@gmail.com>
2026-04-24 15:47:57 +0530

    Clean project without secrets

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "location" {
+  default = "South India"
+}
+
+variable "resource_group_name" {
+  default = "nanda_rg"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  default = "Password123!"
+}
+
+variable "mysql_admin" {
+  default = "nandasql"
+}
+
+variable "mysql_password" {
+  default = "password#123"
+}
+
+
+variable "client_secret" {
+  
+}
