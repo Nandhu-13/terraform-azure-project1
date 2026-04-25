d1e8cf616a265b57c44b631ab1fe92902e7cef8c
NandaKishore <parnatinanda@gmail.com>
2026-04-25 11:48:43 +0530

    final-project

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  features {}
-  client_id       = "40457597-3ef5-41b7-9b98-6ac117c556c4"
-  client_secret = var.client_secret
-  tenant_id       = "6d340aeb-6064-4609-bdd8-fbbc919c143a"
-  subscription_id = "f1a3f80e-bd23-4a11-b0c6-8099b8a34f66"
-}
